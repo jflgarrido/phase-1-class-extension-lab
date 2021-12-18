dae75308ef5d24e50373c75bb7bc5e21f3055a2e
Julius Garrido <jflgarrido@gmail.com>
2021-12-17 19:16:14 -0700

    commit tree 0f57cb67c68664f6edfd7ea0c05375c6d4b33433

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Polygon {
+    constructor(arraySides){
+        this.arraySides = arraySides
+    }
+    get countSides(){
+        return this.arraySides.length
+    }
+    get perimeter(){
+        const sum = this.arraySides.reduce((part_sum, a) => part_sum + a, 0)
+        return sum
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        if(this.arraySides[0] + this.arraySides[1] <= this.arraySides[2] ||
+            this.arraySides[0] + this.arraySides[2] <= this.arraySides[1] ||
+            this.arraySides[1] + this.arraySides[2] <= this.arraySides[0]){
+                return false
+            }
+        return true           
+    }
+}
+class Square extends Polygon {
+    get isValid(){
+        if(this.arraySides[0] === this.arraySides[1] &&
+           this.arraySides[0] === this.arraySides[2] &&
+           this.arraySides[0] === this.arraySides[3]){
+            return true
+        }
+        return false
+    }
+
+    get area(){
+        return Math.pow(this.arraySides[0], 2)
+    }
+}
